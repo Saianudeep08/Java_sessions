d0d7d69dd67ad29e08e05d7c50309fadc27d8305
Sai Anudeep <2311cs010617@mallareddyuniversity.ac.in>
2025-01-05 18:39:34 +0530

    first commit

---
+++ src/App.java
@@ -0,0 +1,12 @@
+public class App {
+     public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.print("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println(4==4);
+        System.out.println(4==6);
+        System.out.println(4<6);
+
+
+    }
+}   
---
+++ src/Session3.java
@@ -0,0 +1,16 @@
+public class Session3 {
+    public static void main(String[] args)  {
+        System.out.println(4==4);
+        System.out.println(4==6);
+        System.out.println(4<6);
+        String str = "Hello";
+        int len =str.length();
+        System.out.println(str.length());
+        System.out.println(str.charAt(0));
+        System.out.println(str.charAt(len -3));
+        System.out.println(str.substring(2,5));
+        String str1 = "Mountain";
+
+
+    }
+}
